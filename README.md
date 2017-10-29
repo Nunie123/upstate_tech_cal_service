b0d48846ddbf9da6c93315dac8c5d47ef1d39a85
Nunie123 <ed.a.nunes@gmail.com>
2017-10-29 14:55:52 -0400

    ean - update to api, now supports meetup and eventbrite

--- README.md
+++ README.md
@@ -1,5 +1,37 @@
 ## Greenville Tech Calendar Service
 
-This application will expose a REST API that will return a JSON of upcoming tech meetups in the Greenville-Spartanburg area.
 
-This API will use the API provided by Code for Greenville [here](https://github.com/codeforgreenville/OpenData/issues/17) to get the list of organizations to be included in the calendar.  It will then make requests to the APIs of the various applications where the meeting data is being hosted (meetup.com, facebook, eventbright, etc.).
+This application provides an endpoint that will return event data for all groups listed
+at https://data.openupstate.org/organizations that host their meeting information on meetup.com
+or eventbrite.com.  To be added added or provide updates to this list, make a comment on the github page here: https://github.com/codeforgreenville/OpenData/issues/18
+
+Meeting hosts currently not supported: Facebook, Nvite, Open Collective, and custom websites.
+
+The exposed JSON includes past and future meetings, as made available by the associated event hosting website APIs.  The data is somewhat large (500+
+  KB), and the endpoint does not currently provide any method for filtering the data returned.
+
+This application saves the data locally and then exposes the locally saved file.  This prevents excessive API calls to the event hosting websites and improves performance.  Once deployed to a public site, the intention is to have the data updated by a cron job on an hourly basis.
+
+The format of the JSON that returns is:
+
+  `[{
+    'event_name': 'Tuesday Hack Night',
+    'group_name': 'Upstate Hackers',
+    'venue': {'name': 'Greenville Open Data HQ',
+              'address': '101 N. Main St., 3rd Floor',
+              'city': 'Greenville',
+              'state': 'SC',
+              'zip': '29605',
+              'country': 'US',
+              'lat': 34.851616,
+              'lon': -82.398392}
+    'url': 'https://data.openupstate.org/',
+    'time': '2017-12-24T15:30:00Z',
+    'rsvp_count': 42,
+    'created_at': '2017-07-04T10:05:00Z',
+    'description': 'Let's meet up and hack everything!',
+    'data_as_of': '2017-10-29T18:29:14Z'
+    }, ...]`
+Note:
+* All timestamps are in UTC.  
+* Some of the description fields include html markup from the host sites.  This application does not verify that there is nothing malicious in the included markup.
--- app.py
+++ app.py
@@ -1,7 +1,17 @@
+'''
+This application provides an endpoint that will return event data for all groups listed
+at https://data.openupstate.org/organizations and host their meeting information on meetup.com
+or eventbrite.com.
+Meeting hosts currently not supported: Facebook, Nvite, Open Collective, and custom websites.
+'''
+
+
 from flask import Flask, jsonify
+import simplejson as json
 from configparser import ConfigParser
 import requests
-import simplejson as json
+import datetime
+
 
 
 
@@ -14,6 +24,19 @@ config.read(config_file)
 app = Flask(__name__)
 app.config['SECRET_KEY'] = config.get('flask','Secret_key')
 
+# Gets list of groups, concatenates meeting lists for all groups, then saves json locally.
+def refresh_all_meetings():
+    meeting_list = get_meeting_list()
+    events = (
+        get_meetup_events(meeting_list['Meetup'])
+        + get_eventbrite_events(meeting_list['Eventbrite'])
+        )
+    with open('all_meetings.json', 'w') as outfile:
+        json.dump(events, outfile)
+    return events
+
+
+# Queries openupstate API for list of groups describing where they host event info
 def get_meeting_list():
     url = 'https://data.openupstate.org/rest/organizations?org_status=active'
     r = requests.get(url)
@@ -25,8 +48,8 @@ def get_meeting_list():
         sorted_sources[source] = [i for i in data if i['field_event_service'] == source and i['field_events_api_key'] != '7709021031'] # this key is for Greer Programmers Group, but is wrong
     return sorted_sources
 
-def get_meetup_meetings():
-    meeting_list = get_meeting_list()['Meetup']
+# Takes list of groups and makes API call to Meetup.com to get event details. Returns list.
+def get_meetup_events(meeting_list):
     group_ids = [i['field_events_api_key'] for i in meeting_list]
     group_ids_str = ','.join(str(group_id) for group_id in group_ids)
     api_key = config.get('meetup','api_key')
@@ -43,7 +66,7 @@ def get_meetup_meetings():
                 'city': event.get('venue').get('city'),
                 'state': event.get('venue').get('state'),
                 'zip': event.get('venue').get('zip'),
-                'country': event.get('venue').get('localized_country_name'),
+                'country': event.get('venue').get('country'),
                 'lat': event.get('venue').get('lat'),
                 'lon': event.get('venue').get('lon')
             }
@@ -56,19 +79,70 @@ def get_meetup_meetings():
             'group_name': event.get('group').get('name'),
             'venue': venue,
             'url': event.get('event_url'),
-            'time': event.get('time'),
+    #note: time is converted from unix timestamp to ISO 8601 timestamp.  This currently works when both the meeting time and local computer time are in same timezone (US/Eastern).  Unsure if it will work when they are in different timezones.
+            'time': datetime.datetime.utcfromtimestamp(int(event.get('time'))/1000).strftime('%Y-%m-%dT%H:%M:%SZ'),
             'rsvp_count': event.get('yes_rsvp_count'),
-            'created_at': event.get('created'),
-            'description': description
+            'created_at': datetime.datetime.utcfromtimestamp(int(event.get('created'))/1000).strftime('%Y-%m-%dT%H:%M:%SZ'),
+            'description': description,
+            'data_as_of': datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
         }
         events.append(event_dict)
     return events
+
+# Takes list of groups and makes API call to EventBrite to get event details. Returns list.
+def get_eventbrite_events(meeting_list):
+    group_ids = [i['field_events_api_key'] for i in meeting_list]
+    token = config.get('eventbrite','token')
+    events=[]
+    for group_id in group_ids:
+        url = 'https://www.eventbriteapi.com/v3/organizers/{}/events/'.format(group_id)
+        r = requests.get(url,headers = {"Authorization": "Bearer {}".format(token)},verify = True)
+        data = json.loads(r.text)
+        if data.get('events'):
+            events_list = data.get('events')
+            venue_ids = []
+            for event in events_list:
+                venue_ids.append(event['venue_id'])
+            venue_ids = list(set(venue_ids))
+            venues={}
+            for venue_id in venue_ids:
+                url = 'https://www.eventbriteapi.com/v3/venues/{}/'.format(venue_id)
+                r = requests.get(url,headers = {"Authorization": "Bearer {}".format(token)},verify = True)
+                data = json.loads(r.text)
+                venue = {
+                    'name': data.get('name'),
+                    'address': '{}, {}'.format(data.get('address').get('address_1'),data.get('address').get('address_2')),
+                    'city': data.get('address').get('city'),
+                    'state': data.get('address').get('state'),
+                    'zip': data.get('address').get('postal_code'),
+                    'country': data.get('address').get('country'),
+                    'lat': data.get('address').get('latitude'),
+                    'lon': data.get('address').get('longitude')
+                }
+                venues[venue_id] = venue
+            group_name = [i['title'] for i in meeting_list if i['field_events_api_key'] == group_id][0]
+            for event in events_list:
+                event_dict = {
+                    'event_name': event.get('name').get('text'),
+                    'group_name': group_name,
+                    'venue': venues[event.get('venue_id')],
+                    'url': event.get('url'),
+                    'time': event.get('start').get("utc"),
+                    'rsvp_count': None,
+                    'created_at': event.get('created'),
+                    'description': event.get('description').get('text'),
+                    'data_as_of': datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+                }
+                events.append(event_dict)
+    return events
+
 
 
 
 @app.route('/', methods=['GET'])
 def get_dates():
-    return jsonify(get_meetup_meetings())
+    with open('all_meetings.json') as json_data:
+        return jsonify(json.load(json_data))
 
 
 if __name__ == '__main__':
