825cecea83d9866d1999f003b8b475f074e74c61
Nunie123 <ed.a.nunes@gmail.com>
2018-04-19 22:27:13 -0400

    ean - added nid and uuid fields to output

--- app.py
+++ app.py
@@ -1,11 +1,3 @@
-'''
-This application provides an endpoint that will return event data for all groups listed
-at https://data.openupstate.org/organizations and host their meeting information on meetup.com
-or eventbrite.com.
-Meeting hosts currently not supported: Facebook, Nvite, Open Collective, and custom websites.
-'''
-
-
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import simplejson as json
@@ -24,7 +16,7 @@ config.read(config_file)
 # instantiate flask app
 app = Flask(__name__)
 CORS(app)
-app.config['SECRET_KEY'] = config.get('flask','secret_key')
+app.config['SECRET_KEY'] = config.get('flask', 'secret_key')
 
 
 # Queries openupstate API for list of groups. Returns dictionary with each source as key (e.g. 'Meetup', 'Eventbrite')
@@ -33,7 +25,7 @@ def get_group_lists():
     r = requests.get(url)
     if r.status_code != 200:
         raise Exception('Could not connect to OpenUpstate API at {}.  Status Code: {}'.format(url, r.status_code))
-    data = json.loads(r.text)    # current meeting sources: '', 'Facebook', 'Nvite', 'Eventbrite', 'Meetup', 'Unknown', 'Open Collective'
+    data = json.loads(r.text)
     all_sources = [x["field_event_service"] for x in data]
     all_sources = list(set(all_sources))    # removes duplicates
     groups_by_source = {}
@@ -41,11 +33,12 @@ def get_group_lists():
         groups_by_source[source] = [i for i in data if i['field_event_service'] == source]
     return groups_by_source
 
+
 # Takes list of groups and makes API call to Meetup.com to get event details. Returns list.
 def get_meetup_events(group_list):
     group_ids = [i['field_events_api_key'] for i in group_list]
     group_ids_str = ','.join(str(group_id) for group_id in group_ids)
-    api_key = config.get('meetup','api_key')
+    api_key = config.get('meetup', 'api_key')
     url = 'https://api.meetup.com/2/events?key={key}&group_id={ids}'.format(key=api_key, ids=group_ids_str)
     r = requests.get(url)
     if r.status_code != 200:
@@ -56,10 +49,14 @@ def get_meetup_events(group_list):
 
 # Takes list of events from Meetup and returns formatted list of events
 def format_meetup_events(events_raw, group_list):
-    events=[]
+    events = []
     for event in events_raw:
         venue_dict = event.get('venue')
-        tags = [i.get('field_org_tags') for i in group_list if i.get('field_events_api_key') == str(event['group']['id'])][0]
+        group_item = [i for i in group_list if i.get('field_events_api_key') == str(event['group']['id'])][0]
+        tags = group_item.get('field_org_tags')
+        uuid = group_item.get('uuid')
+        nid = group_item.get('nid')
+
         if venue_dict:
             venue = {
                 'name': venue_dict.get('name'),
@@ -71,10 +68,13 @@ def format_meetup_events(events_raw, group_list):
                 'lat': venue_dict.get('lat'),
                 'lon': venue_dict.get('lon')
             }
-        else: venue = None
+        else:
+            venue = None
         if event.get('description'):
-            description = event.get('description').replace('<p>','').replace('</p>','')
-        else: description = None
+            description = event.get('description').replace('<p>', '').replace('</p>', '')
+        else:
+            description = None
+
         event_dict = {
             'event_name': event.get('name'),
             'group_name': event.get('group').get('name'),
@@ -89,6 +89,8 @@ def format_meetup_events(events_raw, group_list):
             'created_at': datetime.datetime.utcfromtimestamp(int(event.get('created'))/1000)
                                   .strftime('%Y-%m-%dT%H:%M:%SZ'),
             'description': description,
+            'uuid': uuid,
+            'nid': nid,
             'data_as_of': datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
         }
         events.append(event_dict)
@@ -98,11 +100,11 @@ def format_meetup_events(events_raw, group_list):
 # Takes list of groups hosted on EventBrite and returns list of events.
 def get_eventbrite_events(group_list):
     group_ids = [i['field_events_api_key'] for i in group_list if i['field_events_api_key'] != '']
-    token = config.get('eventbrite','token')
-    events=[]
+    token = config.get('eventbrite', 'token')
+    events = []
     for group_id in group_ids:
         url = 'https://www.eventbriteapi.com/v3/organizers/{}/events/'.format(group_id)
-        r = requests.get(url,headers = {"Authorization": "Bearer {}".format(token)},verify = True)
+        r = requests.get(url, headers={"Authorization": "Bearer {}".format(token)}, verify=True)
         if r.status_code != 200:
             raise Exception('Could not connect to Eventbrite API at {}.  Status Code: {}'.format(url, r.status_code))
         data = json.loads(r.text)
@@ -115,14 +117,14 @@ def get_eventbrite_events(group_list):
 # Takes list of events hosted on EventBrite and returns list of unique venue dictionaries.
 def get_eventbrite_venues(events_list):
     venue_ids = []
-    token = config.get('eventbrite','token')
+    token = config.get('eventbrite', 'token')
     for event in events_list:
         venue_ids.append(event['venue_id'])
     venue_ids = list(set(venue_ids))
-    venues=[]
+    venues = []
     for venue_id in venue_ids:
         url = 'https://www.eventbriteapi.com/v3/venues/{}/'.format(venue_id)
-        r = requests.get(url,headers = {"Authorization": "Bearer {}".format(token)},verify = True)
+        r = requests.get(url, headers={"Authorization": "Bearer {}".format(token)}, verify=True)
         if r.status_code != 200:
             raise Exception('Could not connect to Eventbrite API at {}.  Status Code: {}'.format(url, r.status_code))
         data = json.loads(r.text)
@@ -137,10 +139,11 @@ def format_eventbrite_events(events_list, venues_list, group_list):
     for venue in venues_list:
         venue_id = venue.get('id')
         venue_address = venue.get('address')
+        venue_dict = {}
         if venue_address:
             venue_dict = {
                 'name': venue.get('name'),
-                'address': '{}, {}'.format(venue_address.get('address_1'),venue_address.get('address_2')),
+                'address': '{}, {}'.format(venue_address.get('address_1'), venue_address.get('address_2')),
                 'city': venue_address.get('city'),
                 'state': venue_address.get('state'),
                 'zip': venue_address.get('postal_code'),
@@ -149,9 +152,13 @@ def format_eventbrite_events(events_list, venues_list, group_list):
                 'lon': venue_address.get('longitude')
             }
         venues[venue_id] = venue_dict
+
     for event in events_list:
-        group_name = [i['title'] for i in group_list if i['field_events_api_key'] == event.get('organizer_id')][0]
-        tags = [i['field_org_tags'] for i in group_list if i['field_events_api_key'] == event.get('organizer_id')][0]
+        group_item = [i for i in group_list if i['field_events_api_key'] == event.get('organizer_id')][0]
+        group_name = group_item.get('title')
+        tags = group_item.get('field_org_tags')
+        uuid = group_item.get('uuid')
+        nid = group_item.get('nid')
         event_dict = {
             'event_name': event.get('name').get('text'),
             'group_name': group_name,
@@ -162,6 +169,8 @@ def format_eventbrite_events(events_list, venues_list, group_list):
             'rsvp_count': None,
             'created_at': event.get('created'),
             'description': event.get('description').get('text'),
+            'uuid': uuid,
+            'nid': nid,
             'data_as_of': datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
         }
         events.append(event_dict)
@@ -176,7 +185,7 @@ def parse_date(d):
             eastern = pytz.timezone('US/Eastern')
             date_no_tz = parse(d)
             parsed_date = eastern.localize(date_no_tz, is_dst=None)
-        except:
+        except ValueError:
             return 'Start date {} is in unknown format. '.format(d)
     else:
         return 'Start date {} is in unknown format. '.format(d)
@@ -196,13 +205,13 @@ def filter_events_by_date(events, start_date_str=datetime.datetime.now(datetime.
         return '{}{}'.format(start_date, end_date).replace('None', '')
 
     if start_date and end_date:
-        return [event for event in events if parse(event['time']) >= start_date and parse(event['time']) <= end_date]
+        return [event for event in events if start_date <= parse(event['time']) <= end_date]
     elif start_date:
-        #print('this: {} vs. this: {}'.format(parse(events[0]['time']), start_date))
         return [event for event in events if parse(event['time']) >= start_date]
     elif end_date:
         return [event for event in events if parse(event['time']) <= end_date]
-    else: return events
+    else:
+        return events
 
 
 # Takes list of events and string of tags to return list of events with specified tags
@@ -233,4 +242,4 @@ def get_dates():
 
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
